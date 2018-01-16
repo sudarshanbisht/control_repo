133ddcae329f411a78af56a5ef36609d79f8f42b
sudarshanbisht <sudarshan.bisht2000@gmail.com>
2018-01-16 17:07:39 +0530

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,7 +1,12 @@
 node default {
-	file {'/root/README':
-	ensure => file,
-	content => 'This is test README',
-	owner => root,
-	}
+#	file {'/root/README':
+#	ensure => file,
+#	content => 'This is test README',
+#	owner => root,
+#	}
+}
+
+
+mode 'master.puppet.vm' {
+	include role::master_server
 }
