da747feb8bf19cd0fafb14f701b93852fe2ce284
Bisht, Sudarshan [VPE] <sudarshan.bisht@medtronic.com>
2018-01-23 17:05:43 +0530

    syntax error

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -13,9 +13,9 @@ class minecraft {
 		ensure => file,
 		 content => 'eula=true'
 		 }
-	file {'/etc/systemd/system/minecraft.server':
+	file {'/etc/systemd/system/minecraft.service':
 		ensure => file,
-		source => 'puppet:///modules/minecraft/minecraft.server',
+		source => 'puppet:///modules/minecraft/minecraft.service',
 		 }
 	service {'minecraft':
 		ensure => running,
